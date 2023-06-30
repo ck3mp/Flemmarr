df4f244c602d4f963b6d836f9ce2f92c1398be98
Chris Kemp <chris.kemp@balfourbeatty.com>
2023-06-30 12:50:49 +0100

    Only print errors and move to f strings

--- src/api.py
+++ src/api.py
@@ -16,78 +16,75 @@ class Api(object):
         self.r.mount("http://", adapter)
         self.r.mount("https://", adapter)
 
-    def __url(self, resource="", id=None):
+    def __url(self, resource="", id_var=None):
         address = (
             self.address
             if self.address.startswith("http")
             else "http://" + self.address
         )
-        id_path = "/{}".format(id) if id else ""
-        return "{}:{}{}{}{}".format(address, self.port, self.path, resource, id_path)
+        id_var_path = f"/{id_var}" if id_var else ""
 
-    def __get(self, resource, id=None):
-        response = self.r.get(self.__url(resource, id))
+        return f"{address}:{self.port}{self.path}{resource}{id_var_path}"
+
+    def __get(self, resource, id_var=None):
+        response = self.r.get(self.__url(resource, id_var))
         status_code = response.status_code
 
-        if id:
-            id_string = " {}".format(id)
+        if id_var:
+            id_var_string = f" {id_var}"
         else:
-            id_string = ""
+            id_var_string = ""
 
-        print("Fetching {}{}: {}".format(resource, id_string, status_code))
-        print(response.json())
-        print("\n")
+        print(f"Fetching {resource}{id_var_string}: {status_code}")
+        if status_code > 300:
+            print(response.json())
+            print("\n")
 
         if status_code < 300:
             return response.json()
 
     def __create(self, resource, body):
         response = self.r.post(self.__url(resource), json=body)
-        print(response.json())
-
         status_code = response.status_code
 
         if status_code < 300:
-            print(
-                "Creating {} {}: {}".format(
-                    resource, response.json()["id"], status_code
-                )
-            )
+            print(f"Creating {resource} {response.json()['id_var']}: {status_code}")
         else:
-            print("Creating {}: {}".format(resource, status_code))
+            print(f"Creating {resource}: {status_code}")
 
-        print(response.json())
-        print("\n")
+        if status_code > 300:
+            print(response.json())
+            print("\n")
 
-    def __edit(self, resource, body, id=None):
-        old_version = self.__get(resource, id)
+    def __edit(self, resource, body, id_var=None):
+        old_version = self.__get(resource, id_var)
 
         for key in body:
             old_version[key] = body[key]
 
-        response = self.r.put(self.__url(resource, id), json=old_version)
+        response = self.r.put(self.__url(resource, id_var), json=old_version)
         status_code = response.status_code
 
-        if id:
-            id_string = " {}".format(id)
+        if id_var:
+            id_var_string = f" {id_var}"
         else:
-            id_string = ""
+            id_var_string = ""
 
-        print("Editing {}{}: {}".format(resource, id_string, status_code))
+        print(f"Editing {resource}{id_var_string}: {status_code}")
 
-        print(response.json())
-        print("\n")
+        if status_code > 300:
+            print(response.json())
+            print("\n")
 
-    def __delete(self, resource, id):
-        status_code = self.r.delete(self.__url(resource, id)).status_code
-
-        print("Deleting {} {}: {}".format(resource, id, status_code))
+    def __delete(self, resource, id_var):
+        status_code = self.r.delete(self.__url(resource, id_var)).status_code
+        print(f"Deleting {resource} {id_var}: {status_code}")
 
     def __triage_and_apply(self, object, resource=""):
         if isinstance(object, dict):
             if any(isinstance(object[key], (dict, list)) for key in object):
                 for key in object:
-                    self.__triage_and_apply(object[key], "{}/{}".format(resource, key))
+                    self.__triage_and_apply(object[key], f"{resource}/{key}")
             else:
                 self.__edit(resource, object)
         else:
@@ -95,7 +92,7 @@ class Api(object):
                 self.__create(resource, body)
 
     def initialize(self):
-        response = self.r.get("{}/initialize.js".format(self.__url()))
+        response = self.r.get(f"{self.__url()}/initialize.js")
 
         bits = response.text.split("'")
         api_root = bits[1]
